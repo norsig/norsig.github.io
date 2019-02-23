efc3a9a8a483de5445982fc5a434e35ac23dda2a
root <root@switchcloud.co>
2019-02-23 01:54:22 -0500

    status update

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 cd /var/www/sites/status.norsig.com/public
+rm index.html
+wget http://status.northernsignal.com/
 git add .
 git commit -m "status update"
-git push -u origin master
+git push -u origin master -f
